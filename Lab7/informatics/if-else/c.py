62f7ff556344cb381900816ba9ab6ec929167c4d
Alina Abdullina <fianit8@gmail.com>
2024-03-24 10:52:52 +0300

    add-lab7

---
+++ Lab7/informatics/if-else/c.py
@@ -0,0 +1,16 @@
+system_answer = int(input())
+student_answer = int(input())
+if (system_answer != 1 and student_answer != 1):
+    print("YES")
+else:
+    if (system_answer == 1):
+        if (student_answer == 1):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if (student_answer == 1):
+            print("NO")
+        else:  
+            print("YES")
+  
---
+++ Lab7/informatics/input-output/b.py
@@ -0,0 +1,3 @@
+num = int(input())
+answer = "The next number for the number {} is {}.\nThe previous number for the number {} is {}."
+print(answer.format(num,num+1,num,num-1));
